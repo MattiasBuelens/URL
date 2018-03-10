d038ad8707c7ef5c861fca2a75d1fcc3dc3f256b
Mattias Buelens <mattias@buelens.com>
2018-03-10 12:06:44 +0100

    Set up WPT runner

---
+++ run-web-platform-tests.js
@@ -0,0 +1,26 @@
+'use strict';
+const path = require('path');
+const wptRunner = require('wpt-runner');
+const minimatch = require('minimatch');
+
+const { URL } = require('./url.js');
+
+const testsPath = path.resolve(__dirname, 'web-platform-tests/url');
+const filterGlobs = process.argv.length >= 3 ? process.argv.slice(2) : ['url-constructor.html'];
+
+function filter(testPath) {
+  return filterGlobs.some(glob => minimatch(testPath, glob));
+}
+
+wptRunner(testsPath, { rootURL: 'url/', setup, filter })
+  .then(failures => {
+    process.exitCode = failures;
+  })
+  .catch(e => {
+    console.error(e.stack);
+    process.exitCode = 1;
+  });
+
+function setup(window) {
+  window.URL = URL;
+}
