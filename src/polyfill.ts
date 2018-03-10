db6fad035a4ab3f07e028a5813e55e715ec3e6e9
Mattias Buelens <mattias@buelens.com>
2018-03-10 12:40:43 +0100

    Use let and const

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -5,32 +5,32 @@ const scope = typeof self !== 'undefined' ? self
         : typeof global !== 'undefined' ? global
             : undefined;
 
-var OriginalURL = scope.URL;
+const OriginalURL = scope.URL;
 
 // feature detect for URL constructor
-var hasWorkingUrl = false;
+let hasWorkingUrl = false;
 if (!scope.forceJURL) {
   try {
-    var u = new OriginalURL('b', 'http://a');
+    const u = new OriginalURL('b', 'http://a');
     u.pathname = 'c%20d';
     hasWorkingUrl = u.href === 'http://a/c%20d';
   } catch (e) {
   }
 }
 
-var URL;
+let URL;
 if (hasWorkingUrl) {
   URL = OriginalURL;
 } else {
   URL = jURL;
   // Copy over the static methods
   if (OriginalURL) {
-    jURL.createObjectURL = function (blob) {
+    URL.createObjectURL = function (blob) {
       // IE extension allows a second optional options argument.
       // http://msdn.microsoft.com/en-us/library/ie/hh772302(v=vs.85).aspx
       return OriginalURL.createObjectURL.apply(OriginalURL, arguments);
     };
-    jURL.revokeObjectURL = function (url) {
+    URL.revokeObjectURL = function (url) {
       OriginalURL.revokeObjectURL(url);
     };
   }
