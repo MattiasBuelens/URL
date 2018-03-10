29330d369f27af725a22967941b63061f82830fc
Mattias Buelens <mattias@buelens.com>
2018-03-10 12:17:17 +0100

    Fix path to main file

--- run-web-platform-tests.js
+++ run-web-platform-tests.js
@@ -3,7 +3,7 @@ const path = require('path');
 const wptRunner = require('wpt-runner');
 const minimatch = require('minimatch');
 
-const { URL } = require('./url.js');
+const { URL } = require('./dist/url.js');
 
 const testsPath = path.resolve(__dirname, 'web-platform-tests/url');
 const filterGlobs = process.argv.length >= 3 ? process.argv.slice(2) : ['url-constructor.html'];
