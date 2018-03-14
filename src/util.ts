0ba6d37936ad6743a4580ea863853af49aac595d
Mattias Buelens <mattias@buelens.com>
2018-03-15 00:55:29 +0100

    Fix indentation

--- src/util.ts
+++ src/util.ts
@@ -22,26 +22,26 @@ export function sequenceToArray<T>(x: Iterable<T>): T[] {
 }
 
 export const getCodePointAt: (input: string, index: number) => number | undefined
-= (typeof String.prototype.codePointAt === 'function')
-? (input, index) => input.codePointAt(index)
-: (input, index) => {
-  const size = input.length;
-  // Get the first code unit
-  let first = input.charCodeAt(index);
-  let second: number;
-  // check if it’s the start of a surrogate pair
-  if (
-      first >= 0xD800 && first <= 0xDBFF && // high surrogate
-      size > index + 1 // there is a next code unit
-  ) {
-    second = input.charCodeAt(index + 1);
-    if (second >= 0xDC00 && second <= 0xDFFF) { // low surrogate
-      // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-      return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
-    }
-  }
-  return first;
-};
+    = (typeof String.prototype.codePointAt === 'function')
+    ? (input, index) => input.codePointAt(index)
+    : (input, index) => {
+      const size = input.length;
+      // Get the first code unit
+      let first = input.charCodeAt(index);
+      let second: number;
+      // check if it’s the start of a surrogate pair
+      if (
+          first >= 0xD800 && first <= 0xDBFF && // high surrogate
+          size > index + 1 // there is a next code unit
+      ) {
+        second = input.charCodeAt(index + 1);
+        if (second >= 0xDC00 && second <= 0xDFFF) { // low surrogate
+          // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+        }
+      }
+      return first;
+    };
 
 export function getCodePoints(input: string): number[] {
   const result: number[] = [];
@@ -61,38 +61,38 @@ const MAX_SIZE = 0x4000;
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint#Polyfill
 export const fromCodePoints: (codePoints: number[]) => string
-= (typeof String.fromCodePoint === 'function')
-? codePoints => String.fromCodePoint(...codePoints)
-: (codePoints) => {
-  const codeUnits: number[] = [];
-  const length = codePoints.length;
-  let result = '';
-  for (let index = 0; index < length; index++) {
-    let codePoint = codePoints[index];
-    if (
-        !isFinite(codePoint) || // `NaN`, `+Infinity`, or `-Infinity`
-        codePoint < 0 || // not a valid Unicode code point
-        codePoint > 0x10FFFF || // not a valid Unicode code point
-        Math.floor(codePoint) !== codePoint // not an integer
-    ) {
-      throw RangeError(`Invalid code point: ${codePoint}`);
-    }
-    if (codePoint <= 0xFFFF) { // BMP code point
-      codeUnits.push(codePoint);
-    } else { // Astral code point; split in surrogate halves
-      // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-      codePoint -= 0x10000;
-      let highSurrogate = (codePoint >> 10) + 0xD800;
-      let lowSurrogate = (codePoint % 0x400) + 0xDC00;
-      codeUnits.push(highSurrogate, lowSurrogate);
-    }
-    if (index + 1 === length || codeUnits.length > MAX_SIZE) {
-      result += String.fromCharCode(...codeUnits);
-      codeUnits.length = 0;
-    }
-  }
-  return result;
-};
+    = (typeof String.fromCodePoint === 'function')
+    ? codePoints => String.fromCodePoint(...codePoints)
+    : (codePoints) => {
+      const codeUnits: number[] = [];
+      const length = codePoints.length;
+      let result = '';
+      for (let index = 0; index < length; index++) {
+        let codePoint = codePoints[index];
+        if (
+            !isFinite(codePoint) || // `NaN`, `+Infinity`, or `-Infinity`
+            codePoint < 0 || // not a valid Unicode code point
+            codePoint > 0x10FFFF || // not a valid Unicode code point
+            Math.floor(codePoint) !== codePoint // not an integer
+        ) {
+          throw RangeError(`Invalid code point: ${codePoint}`);
+        }
+        if (codePoint <= 0xFFFF) { // BMP code point
+          codeUnits.push(codePoint);
+        } else { // Astral code point; split in surrogate halves
+          // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          codePoint -= 0x10000;
+          let highSurrogate = (codePoint >> 10) + 0xD800;
+          let lowSurrogate = (codePoint % 0x400) + 0xDC00;
+          codeUnits.push(highSurrogate, lowSurrogate);
+        }
+        if (index + 1 === length || codeUnits.length > MAX_SIZE) {
+          result += String.fromCharCode(...codeUnits);
+          codeUnits.length = 0;
+        }
+      }
+      return result;
+    };
 
 export function compareArrays(arr1: number[], arr2: number[]): number {
   const length1 = arr1.length;
