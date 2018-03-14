41bfc05ef6d1b3fcd3ab3a4444ba321661ffc824
Mattias Buelens <mattias@buelens.com>
2018-03-15 00:53:20 +0100

    Add fromCodePoints polyfill

--- src/util.ts
+++ src/util.ts
@@ -54,6 +54,40 @@ export function getCodePoints(input: string): number[] {
   return result;
 }
 
+const MAX_SIZE = 0x4000;
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint#Polyfill
+export function fromCodePoints(codePoints: number[]): string {
+  const codeUnits: number[] = [];
+  const length = codePoints.length;
+  let result = '';
+  for (let index = 0; index < length; index++) {
+    let codePoint = codePoints[index];
+    if (
+        !isFinite(codePoint) || // `NaN`, `+Infinity`, or `-Infinity`
+        codePoint < 0 || // not a valid Unicode code point
+        codePoint > 0x10FFFF || // not a valid Unicode code point
+        Math.floor(codePoint) !== codePoint // not an integer
+    ) {
+      throw RangeError(`Invalid code point: ${codePoint}`);
+    }
+    if (codePoint <= 0xFFFF) { // BMP code point
+      codeUnits.push(codePoint);
+    } else { // Astral code point; split in surrogate halves
+      // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+      codePoint -= 0x10000;
+      let highSurrogate = (codePoint >> 10) + 0xD800;
+      let lowSurrogate = (codePoint % 0x400) + 0xDC00;
+      codeUnits.push(highSurrogate, lowSurrogate);
+    }
+    if (index + 1 === length || codeUnits.length > MAX_SIZE) {
+      result += String.fromCharCode(...codeUnits);
+      codeUnits.length = 0;
+    }
+  }
+  return result;
+}
+
 export function compareArrays(arr1: number[], arr2: number[]): number {
   const length1 = arr1.length;
   const length2 = arr1.length;
