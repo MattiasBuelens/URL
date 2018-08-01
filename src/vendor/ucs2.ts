e38e5f147666c37d8b06cf2225af203a959b9883
Mattias Buelens <mattias@buelens.com>
2018-08-02 00:12:14 +0200

    Import ucs2decode from punycode

--- src/vendor/ucs2.ts
+++ src/vendor/ucs2.ts
@@ -2,30 +2,7 @@
 
 import { fromCodeUnits } from "../util";
 
-// Taken from https://mths.be/punycode
-export function ucs2decode(string: string): number[] {
-  const output: number[] = [];
-  let counter = 0;
-  const length = string.length;
-  while (counter < length) {
-    const value = string.charCodeAt(counter++);
-    if (value >= 0xD800 && value <= 0xDBFF && counter < length) {
-      // high surrogate, and there is a next character
-      const extra = string.charCodeAt(counter++);
-      if ((extra & 0xFC00) === 0xDC00) { // low surrogate
-        output.push(((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000);
-      } else {
-        // unmatched surrogate; only append this code unit, in case the next
-        // code unit is the high surrogate of a surrogate pair
-        output.push(value);
-        counter--;
-      }
-    } else {
-      output.push(value);
-    }
-  }
-  return output;
-}
+export { ucs2decode } from 'punycode';
 
 // Taken from https://mths.be/punycode
 export function ucs2encode(array: number[]): string {
